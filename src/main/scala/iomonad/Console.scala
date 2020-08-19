265e976c46aa4c88cf9fcd6fbc985cc2d6f3ba6a
cclcmj <MJ1666812276@outlook.com>
2020-08-19 11:40:55 +0800

    增加ConsoleState

--- src/main/scala/iomonad/Console.scala
+++ src/main/scala/iomonad/Console.scala
@@ -1,7 +1,7 @@
 /*
  * @Author: mj
  * @Date: 2020-08-14 10:41:32
- * @LastEditTime: 2020-08-16 09:22:17
+ * @LastEditTime: 2020-08-19 11:39:41
  * @LastEditors: Please set LastEditors
  * @Description: 一个支持控制台I/O的monad
  * @FilePath: /iomonad/src/main/scala/iomonad/Console.scala
@@ -23,7 +23,7 @@ trait Console[A] {
    */  
   def toThunk:()=>A
 }
-case object ReadLine extends Console[Option[String]]{
+case class  ReadLine() extends Console[Option[String]]{
   override def toPar = Par.lazyUnit(run)
   override def toThunk: () => Option[String] = ()=>run
 /**
@@ -41,7 +41,7 @@ object Console{
   import Free._
   import Translate._
   type ConsoleIO[A] = Free[Console,A]
-  def readLn:ConsoleIO[Option[String]] = FSuspend(ReadLine)
+  def readLn:ConsoleIO[Option[String]] = FSuspend(ReadLine())
   def printLn(line:String):ConsoleIO[Unit] = FSuspend(PrintLine(line))
   val consoleToFunction0 = new (Console~>Function0){def apply[A](a: Console[A]): () => A = a.toThunk}
   val consoleToPar = new (Console~>Par) {def apply[A](a: Console[A]): Par.Par[A] = a.toPar}
@@ -62,7 +62,7 @@ object Console{
   def runConsole[A](a:Free[Console,A]):A = 
     runTrampoline(translate(a)(consoleToFunction0))
 }
-/**
+/*
  * @description: F[A]到G[A]的转化类型
  */    
 trait Translate[F[_],G[_]] {def apply[A](f:F[A]):G[A]}
@@ -77,7 +77,6 @@ case class ConsoleReader[A](run:String=>A){
   def map[B](f:A=>B):ConsoleReader[B] = ConsoleReader(r=>f(run(r)))
   def flatMap[B](f:A=>ConsoleReader[B]):ConsoleReader[B] = ConsoleReader(r=>f(run(r)).run(r))
 }
-
 object ConsoleReader {
   import Free._
   import Translate._
@@ -97,4 +96,40 @@ object ConsoleReader {
     }
   def runConsoleReader[A](io:ConsoleIO[A]):ConsoleReader[A] = 
     runFree[Console,ConsoleReader,A](io)(consoleToReader)
+}
+/**
+ * @description: 
+ * 为了使用更完整的控制台IO模拟，编写一个解释器并使用两个list，一个代表输入缓存，一个代表输出缓存
+ * 当解释器遇到一个ReadLine时，可以从输入缓存中弹出一个元素，当遇到一个PrintLine时，可以向输出缓存中压入一个元素
+ */
+case class Buffers(in: List[String],out:List[String])
+case class ConsoleState[A](run: Buffers => (A,Buffers)){}
+object ConsoleState{
+  import Free._
+  import Translate._
+  import Console._
+  implicit class ConsoleS[A](console:Console[A]){
+    def toState:ConsoleState[A] = ???
+  }
+  implicit class ReadLineS(r:ReadLine){
+    def toState = ConsoleState { bufs =>
+      bufs.in match {
+        case List() => (None, bufs)
+        case h :: t => (Some(h), bufs.copy(in = t))
+      }
+    }
+  }
+  implicit class PrintLineS(p:PrintLine) {
+    def toState = ConsoleState { bufs => ((), bufs.copy(out = bufs.out :+ p.line)) } // append to the output
+
+  }
+  implicit val stateMonad:Monad[ConsoleState] = new Monad[ConsoleState]{
+    def unit[A](a: => A): ConsoleState[A] = ???
+    def flatMap[A, B](fa: ConsoleState[A])(f: A => ConsoleState[B]): ConsoleState[B] = ???
+  }
+  val consoleToState = new (Console ~> ConsoleState){
+    def apply[A](c: Console[A]): ConsoleState[A] = c.toState
+  }
+  def runConsoleState[A](io:ConsoleIO[A]):ConsoleState[A] = 
+    runFree[Console,ConsoleState,A](io)(consoleToState)
 }
