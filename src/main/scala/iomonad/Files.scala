850af666550412e31d11a4e56307d3b18ed408db
杰 ccclc <MJ1666812276@outlook.com>
2020-08-23 02:15:03 +0000

    13.7为什么IO类型不足以支撑流式IO

---
+++ src/main/scala/iomonad/Files.scala
@@ -0,0 +1,21 @@
+package iomonad
+
+
+trait Files[A]
+trait HandleR
+trait HandleW
+case class OpenRead(file:String) extends Files[HandleR]
+case class OpenWrite(file:String) extends Files[HandleW]
+case class FReadLine(h:HandleR) extends Files[Option[String]]
+case class FWriteLine(h:HandleW,line:String) extends Files[Unit]
+object Files{
+    def loop(f:HandleR,c:HandleW):Free[Files,Unit] = for{
+        line <- FSuspend{ FReadLine(f) }
+        _ <- line match{
+            case None => FSuspend(FWriteLine(c,""))
+            case Some(s)=> FSuspend{
+                                FWriteLine(c,TemperatureConverter.fahrenheitToCelsius(s.toDouble).toString())
+                            } flatMap (_=> loop(f,c))
+        }
+    } yield ()
+}
