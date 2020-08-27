5b74701d16b82a91733026aa6a7e0e06ccfab132
杰 ccclc <MJ1666812276@outlook.com>
2020-08-27 09:29:43 +0000

    完成Process创建

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,7 @@
 import localEffects.STImmutable
 import java.util.concurrent._
 import iomonad._
-import streamingIO.CountLines
+import streamingIO._
 object AppIO {
   def main(args:Array[String]):Unit = {
     val pool = Executors.newFixedThreadPool(8)
@@ -12,6 +12,11 @@ object AppIO {
     //  _ <- Console.printLn(STImmutable.quicksort(3::1::2::Nil).toString)
     //}yield ()
     //Console.translate(c)(Console.consoleToPar)
-    CountLines.linesGt40k(" ")
+    val p = Process.lift((x:Int)=>x*2)
+    val xs = p(Stream(1,2,3))
+    val c = for{
+      _ <- Console.printLn(xs.take(5).toList.toString)
+    } yield ()
+    Console.translate(c)(Console.consoleToPar)
   }
 }
--- src/main/scala/streamingIO/Process.scala
+++ src/main/scala/streamingIO/Process.scala
@@ -1,7 +1,7 @@
 /*
  * @Author: mj
  * @Date: 2020-08-26 07:32:18
- * @LastEditTime: 2020-08-26 08:08:43
+ * @LastEditTime: 2020-08-27 09:24:11
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /iomonad/src/main/scala/streamingIO/Process.scala
@@ -13,14 +13,40 @@ package streamingIO
  * 一个Process可以有三种种状态：Emit、Await、Halt。（每个都是驱动的信号）
 */
 sealed trait Process[I,O] {
-    def apply(s:Stream[I]):Stream[O] = this match {
+    //apply是Process与流的结合，是三种状态的Process处理每一个流中的元素
+    //即Process(h(下一个元素),t(下一个Process))(s)转变为f(h)#::t(s)
+    def apply(s:Stream[I]):Stream[O] ={ this match {
         case Await(recv) => s match {
             case h#::t => recv(Some(h))(t)
             case xs => recv(None)(xs)//stream 是空的
         }
         case Emit(h, t) => h #:: t(s)
         case Halt() => Stream()
+        }
     }
+    // repeat 是由执行前一个Process转换为执行下一个Process的工具
+    //与liftone相结合 做到
+    //go(await(f))(s)=>emit(f(h),go(halt()))(s)
+    //=>fh#::go(await(f))(None)=>fh#::halt()(None)结束
+    def repeat:Process[I,O] = {
+        def go(p:Process[I,O]):Process[I,O] = p match {
+            case Halt() => go(this)
+            case Await(recv) => Await{
+                case None => recv(None)
+                case i => go(recv(i))
+            }
+            case Emit(h, t) => Emit(h,go(t))
+        }
+        go(this)
+    }
+}
+object Process{
+    //将f提升为Process，添加状态转换逻辑
+    def liftOne[I,O](f:I=>O):Process[I,O] = Await{
+        case Some(value) => Emit(f(value))
+        case None => Halt()
+    }
+    def lift[I,O](f:I=>O):Process[I,O] = liftOne(f).repeat
 }
 //Emit告诉驱动器将head值递送给输出流，而后tail的部分继续由状态机处理
 case class Emit[I,O](head:O,tail:Process[I,O] = Halt[I,O]()) extends Process[I,O]
