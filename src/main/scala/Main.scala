1e676f2f0f749a10925a4d618fdf25ff32917989
cclcmj <MJ1666812276@outlook.com>
2020-09-09 14:08:13 +0800

    环境

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -10,6 +10,7 @@ object AppIO {
   def pureMain(args:IndexedSeq[String]):IO[_] ={ 
     val p1 = Process.lift((d:Double)=>d.toString()+"p1p1p1p1p1") 
     val f1 =  (s:String)=>Process.lift((d:Double)=>s+d.toString+"f1f1f1f1f1f1")
+    val f2 =  (s:String)=>Process.lift((d:Double)=>s+d.toString+"f2f2f2f2f2f2")
     val resultP = (p1 flatMap f1 flatMap f2) (Stream(1.0,4.0,6.0,7.0))
     val c =  Console.printLn(resultP.toList.toString())
     Console.translate(c)(Console.consoleToPar)
